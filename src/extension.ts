8efeb607e43fdea277ca861284d33542a5841c39
SYu449 <sy449@drexel.edu>
2021-09-02 17:10:00 -0400

    latest

--- src/extension.ts
+++ src/extension.ts
@@ -87,22 +87,22 @@ export async function activate(context: vscode.ExtensionContext) {
           const fid = metrics.FID ? (metrics.FID / 1000).toFixed(2) : 'N/A';
           const hydration = (metrics['Next.js-hydration'] / 1000).toFixed(2);
           const ttfb = (metrics.TTFB / 1000).toFixed(2);
-          const fcp_score = isNaN(Number(fcp)) ? 'None ⚫️' : Number(fcp) < 1.8 ? 'Good 🟢' : Number(fcp) < 3 ? 'Okay 🟠' : 'Poor 🔴';
-          const cls_score = isNaN(Number(cls)) ? 'None ⚫️' : Number(cls) < 0.1 ? 'Good 🟢' : Number(cls) < 0.25 ? 'Okay 🟠' : 'Poor 🔴';
-          const lcp_score = isNaN(Number(lcp)) ? 'None ⚫️' : Number(lcp) < 2.5 ? 'Good 🟢' : Number(lcp) < 4 ? 'Okay 🟠' : 'Poor 🔴';
-          const fid_score = isNaN(Number(fid)) ? 'None ⚫️' : Number(fid) < 1 ? 'Good 🟢' : Number(fid) < 3 ? 'Okay 🟠' : 'Poor 🔴';
-          const ttfb_score = isNaN(Number(ttfb)) ? 'None ⚫️' : Number(ttfb) < 0.6 ? 'Good 🟢' : 'Poor 🔴';
+          const fcp_score = isNaN(Number(fcp)) ? 'N/A  ⚫️' : Number(fcp) < 1.8 ? 'Good 🟢' : Number(fcp) < 3 ? 'Moderate 🟠' : 'Poor 🔴';
+          const cls_score = isNaN(Number(cls)) ? 'N/A  ⚫️' : Number(cls) < 0.1 ? 'Good 🟢' : Number(cls) < 0.25 ? 'Moderate 🟠' : 'Poor 🔴';
+          const lcp_score = isNaN(Number(lcp)) ? 'N/A  ⚫️' : Number(lcp) < 2.5 ? 'Good 🟢' : Number(lcp) < 4 ? 'Moderate 🟠' : 'Poor 🔴';
+          const fid_score = isNaN(Number(fid)) ? 'N/A  ⚫️' : Number(fid) < 1 ? 'Good 🟢' : Number(fid) < 3 ? 'Moderate 🟠' : 'Poor 🔴';
+          const ttfb_score = isNaN(Number(ttfb)) ? 'N/A  ⚫️' : Number(ttfb) < 0.6 ? 'Good 🟢' : 'Poor 🔴';
 
           const fcp_avg = (avg.FCP / 1000).toFixed(2);
           const cls_avg = avg.CLS.toFixed(2);
           const lcp_avg = (avg.LCP / 1000).toFixed(2);
           const fid_avg = avg.FID ? (avg.FID / 1000).toFixed(2) : 'N/A';
           const ttfb_avg = (avg.TTFB / 1000).toFixed(2);
-          const fcp_avg_score = isNaN(Number(fcp_avg)) ? 'None ⚫️' : Number(fcp_avg) < 1.8 ? 'Good 🟢' : Number(fcp_avg) < 3 ? 'Okay 🟠' : 'Poor 🔴';
-          const cls_avg_score = isNaN(Number(cls_avg)) ? 'None ⚫️' : Number(cls_avg) < 0.1 ? 'Good 🟢' : Number(cls_avg) < 0.25 ? 'Okay 🟠' : 'Poor 🔴';
-          const lcp_avg_score = isNaN(Number(lcp_avg)) ? 'None ⚫️' : Number(lcp_avg) < 2.5 ? 'Good 🟢' : Number(lcp_avg) < 4 ? 'Okay 🟠' : 'Poor 🔴';
-          const fid_avg_score = isNaN(Number(fid_avg)) ? 'None ⚫️' : Number(fid_avg) < 1 ? 'Good 🟢' : Number(fid_avg) < 3 ? 'Okay 🟠' : 'Poor 🔴';
-          const ttfb_avg_score = isNaN(Number(ttfb_avg)) ? 'None ⚫️' : Number(ttfb_avg) < 0.6 ? 'Good 🟢' : 'Poor 🔴';
+          const fcp_avg_score = isNaN(Number(fcp_avg)) ? 'N/A  ⚫️' : Number(fcp_avg) < 1.8 ? 'Good 🟢' : Number(fcp_avg) < 3 ? 'Moderate 🟠' : 'Poor 🔴';
+          const cls_avg_score = isNaN(Number(cls_avg)) ? 'N/A  ⚫️' : Number(cls_avg) < 0.1 ? 'Good 🟢' : Number(cls_avg) < 0.25 ? 'Moderate 🟠' : 'Poor 🔴';
+          const lcp_avg_score = isNaN(Number(lcp_avg)) ? 'N/A  ⚫️' : Number(lcp_avg) < 2.5 ? 'Good 🟢' : Number(lcp_avg) < 4 ? 'Moderate 🟠' : 'Poor 🔴';
+          const fid_avg_score = isNaN(Number(fid_avg)) ? 'N/A  ⚫️' : Number(fid_avg) < 1 ? 'Good 🟢' : Number(fid_avg) < 3 ? 'Moderate 🟠' : 'Poor 🔴';
+          const ttfb_avg_score = isNaN(Number(ttfb_avg)) ? 'N/A  ⚫️' : Number(ttfb_avg) < 0.6 ? 'Good 🟢' : 'Poor 🔴';
 
           const fcp_link = 'https://web.dev/fcp/ ';
           const cls_link = 'https://web.dev/cls/ ';
@@ -110,15 +110,15 @@ export async function activate(context: vscode.ExtensionContext) {
           const fid_link = 'https://web.dev/fid/ ';
           const ttfb_link = 'https://web.dev/time-to-first-byte/ ';
           const helpFixScore = `Want to improve "poor" areas?: ${fcp_score === 'Poor 🔴' ? fcp_link : ""}${cls_score === 'Poor 🔴' ? cls_link : ""}${fid_score === 'Poor 🔴' ? fid_link : ""}${lcp_score === 'Poor 🔴' ? lcp_link : ""}${ttfb_score === 'Poor 🔴' ? ttfb_link : ""}`;   
-          const metricOutput = `-------------------------------
-Metric | Value   | Average
--------------------------------
-FCP:   | ${fcp + 's'}${' '.repeat(7 - fcp.length)} ${fcp_score}  | ${fcp_avg}
+          const metricOutput = `------------------------------------------------
+Metric | Value         | Moving Average (Last 5)
+------------------------------------------------
+FCP:   | ${fcp + 's'}${' '.repeat(7 - fcp.length)} ${fcp_score}  | ${fcp_avg} ${fcp_avg_score}
 CLS:   | ${cls}${' '.repeat(8 - cls.length)} ${cls_score}  | ${cls_avg}
 LCP:   | ${lcp + 's'}${' '.repeat(7 - lcp.length)} ${lcp_score}  | ${lcp_avg}
 FID:   | ${fid + 's'}${' '.repeat(7 - fid.length)} ${fid_score}  | ${fid_avg}
 TTFB:  | ${ttfb + 's'}${' '.repeat(7 - ttfb.length)} ${ttfb_score}  | ${ttfb_avg}
--------------------------------\n`;
+-----------------------------------------------\n`;
           output.clear();
           output.show();
           output.appendLine(metricOutput);
