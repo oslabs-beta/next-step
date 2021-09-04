a1d4a6944cf14a7f3a3cf2c43fe312e005df1af5
SYu449 <sy449@drexel.edu>
2021-09-04 14:42:11 -0400

    0.1.1

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -3,7 +3,7 @@ import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
 export function run(): Promise<void> {
-	// Create the mocha test
+	// Create the mocha test.
 	const mocha = new Mocha({
 		ui: 'tdd',
 		color: true
